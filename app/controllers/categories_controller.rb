7c192fdd0e1b95edda37c4fe5c9ec28fb6fbb72a
Just Me <just806me@gmail.com>
2017-05-10 00:09:00 +0300

    fix permit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   private
   def resource_params
-    params.require(:category).permit(:name, :visible)
+    params.require(:category).permit(:name, :visible, :income, :slug)
   end
 end
