0ae956b5758bb3577159019728d52c5d940fefca
Alexander Tkachuk <mail@tksasha.me>
2017-06-01 20:29:43 +0300

    cleanup ItemsController

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,31 +1,12 @@
 class ItemsController < ApplicationController
+  include ActsAsRESTController
+
   helper_method :items, :consolidates
 
-  #
-  # TODO: add push state (change URL by ajax)
-  #
-
-  def create
-    @item = Item.new resource_params
- 
-    render :new unless resource.save
-  end
-
-  #
-  # TODO: implement errors.js.erb for update.js
-  #
-
-  def destroy
-    resource.destroy
-  end
-
   private
+  # TODO: spec me
   def collection
     items DateRange.new(DateFactory.build params).month
-  end
-
-  def resource
-    @item ||= Item.find(params[:id])
   end
 
   def items date_range
@@ -36,6 +17,7 @@ class ItemsController < ApplicationController
     @consolidates ||= Consolidate.by DateRange.new(DateFactory.build params).month
   end
 
+  # TODO: spec me
   def resource_params
     params.require(:item).permit(:date, :formula, :category_id, :description)
   end
--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -7,32 +7,6 @@ RSpec.describe ItemsController, type: :controller do
     it { should render_template :index }
   end
 
-  describe '#create.js' do
-    let(:item) { double }
-
-    let(:params) do
-      { item: { date: '2014-04-22', formula: '2+2', category_id: '1', description: 'Buys' } }
-    end
-
-    before { expect(Item).to receive(:new).with(permit! params[:item]).and_return(item) }
-
-    context do
-      before { expect(item).to receive(:save).and_return(true) }
-
-      before { post :create, params: params, format: :js }
-
-      it { should render_template :create }
-    end
-
-    context do
-      before { expect(item).to receive(:save).and_return(false) }
-
-      before { post :create, params: params, format: :js }
-
-      it { should render_template :new }
-    end
-  end
-
   describe '#items' do
     let(:item) { stub_model Item }
 
@@ -52,41 +26,5 @@ RSpec.describe ItemsController, type: :controller do
     end
 
     it { expect { subject.send :items, date_range }.to_not raise_error }
-  end
-
-  describe '#resource' do
-    before { expect(subject).to receive(:params).and_return({ id: 43 }) }
-
-    before { expect(Item).to receive(:find).with(43).and_return(:resource) }
-
-    its(:resource) { should eq :resource }
-  end
-
-  describe '#update.js' do
-    let(:item) { double }
-
-    let(:params) do
-      { item: { date: '2014-04-22', formula: '2+2', category_id: '1', description: 'Buys' }, id: 1 }
-    end
-
-    before { subject.instance_variable_set :@item, item }
-
-    before { expect(item).to receive(:update!).with(permit! params[:item]) }
-
-    before { put :update, params: params, format: :js }
-
-    it { should render_template :update }
-  end
-
-  describe '#destroy.js' do
-    let(:item) { double }
-
-    before { subject.instance_variable_set :@item, item }
-
-    before { expect(item).to receive(:destroy) }
-
-    before { delete :destroy, params: { id: 13 }, format: :js }
-
-    it { should render_template :destroy }
   end
 end
