68f31ba20a8b581bedbac12d7df9a9e8d395c788
Alexander Tkachuk <mail@tksasha.me>
2019-04-24 17:35:35 +0300

    removed ActsAsRESTController; refactored ItemsController

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,17 @@
 class ItemsController < ApplicationController
-  include ActsAsRESTController
-
   helper_method :items, :consolidates
+
+  def create
+    render :new, status: 422 unless resource.save
+  end
+
+  def update
+    render :edit, status: 422 unless resource.update resource_params
+  end
+
+  def destroy
+    resource.destroy
+  end
 
   private
   # TODO: spec me
@@ -13,12 +23,20 @@ class ItemsController < ApplicationController
     @items ||= Item.search(date_range, params[:category]).includes(:category)
   end
 
+  # TODO: spec me
   def consolidates
     @consolidates ||= Consolidate.by DateRange.new(DateFactory.build params).month
   end
 
-  # TODO: spec me
   def resource_params
     params.require(:item).permit(:date, :formula, :category_id, :description)
+  end
+
+  def resource
+    @resource ||= Item.find params[:id]
+  end
+
+  def build_resource
+    @resource = Item.new resource_params
   end
 end
--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -27,4 +27,56 @@ RSpec.describe ItemsController, type: :controller do
 
     it { expect { subject.send :items, date_range }.to_not raise_error }
   end
+
+  describe '#resource_params' do
+    let :params do
+      acp item: { date: nil, formula: nil, category_id: nil, description: nil }
+    end
+
+    before { expect(subject).to receive(:params).and_return(params) }
+
+    its(:resource_params) { should eq params[:item].permit! }
+  end
+
+  describe '#resource' do
+    context do
+      before { subject.instance_variable_set :@resource, :resource }
+
+      its(:resource) { should eq :resource }
+    end
+
+    context do
+      before { expect(subject).to receive(:params).and_return({ id: 26 }) }
+
+      before { expect(Item).to receive(:find).with(26).and_return(:resource) }
+
+      its(:resource) { should eq :resource }
+    end
+  end
+
+  describe '#build_resource' do
+    before { expect(subject).to receive(:resource_params).and_return(:resource_params) }
+
+    before { expect(Item).to receive(:new).with(:resource_params).and_return(:resource) }
+
+    before { subject.send :build_resource }
+
+    its(:resource) { should eq :resource }
+  end
+
+  it_behaves_like :create, format: :js do
+    let(:success) { -> { should render_template(:create).with_status(201) } }
+
+    let(:failure) { -> { should render_template(:new).with_status(422) } }
+  end
+
+  it_behaves_like :update, format: :js do
+    let(:success) { -> { should render_template(:update).with_status(200) } }
+
+    let(:failure) { -> { should render_template(:edit).with_status(422) } }
+  end
+
+  it_behaves_like :destroy, format: :js do
+    let(:success) { -> { should render_template(:destroy).with_status(200) } }
+  end
 end
