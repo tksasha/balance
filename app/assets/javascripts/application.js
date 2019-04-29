83b19f8363b51322431bc178af1a08153a81c871
Alexander Tkachuk <mail@tksasha.me>
2019-04-29 16:52:24 +0300

    refactored header

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,25 +1,15 @@
 // = require jquery
 // = require jquery_ujs
-// = require popper
-// = require bootstrap-sprockets
-//
-// = require bootstrap-datepicker/core
-// = require bootstrap-datepicker/locales/bootstrap-datepicker.ua
+// = require materialize
 //
 // = require_tree .
 //
 // = require_self
 
-var BOOTSTRAP_DATEPICKER_DEFAULTS = { format: 'dd.mm.yyyy', autoclose: true, language: 'ua', todayHighlight: true };
-
 $(function() {
   // Global AJAX-events
   $(document).ajaxStart(function() { $('#ajax-loader').show() });
   $(document).ajaxStop(function() { $('#ajax-loader').hide() });
-
-  $('.bootstrap-datepicker').datepicker(BOOTSTRAP_DATEPICKER_DEFAULTS);
-
-  //$('select').selectpicker();
 
   $('body').on('created.category', CreateOrUpdateCategoryCallback);
 
