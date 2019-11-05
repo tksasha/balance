4637d9142f74ac126a73e94b57d464c391345586
Alexander Tkachuk <mail@tksasha.me>
2019-11-05 15:41:03 +0200

    used `items_path` in the `header`

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@
 Rails.application.routes.draw do
   resources :items, only: %i[index create edit update destroy]
 
-  constraints year: /\d{4}/, month: /\d{1,2}/, day: /\d{1,2}/, category: /[a-z_-]+/ do
-    get '/(:year(/:month)(/:category))' => 'items#index'
-  end
-
   resources :cashes, :categories
 
   resource :backoffice, only: :show, controller: :backoffice
