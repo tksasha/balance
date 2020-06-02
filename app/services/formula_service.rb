3ea305fb5eefdfbc73a8124b1ea1f1ec33ef53b2
Oleksandr Tkachuk <mail@tksasha.me>
2020-06-02 12:20:59 +0300

    gems were updated; fixed Rubocop warnings

--- app/services/formula_service.rb
+++ app/services/formula_service.rb
@@ -7,12 +7,12 @@ class FormulaService
       string = \
         string
         .to_s
-        .gsub(/[^0-9\+\-\.\*]+/, '') # only digits, dots, pluses, multiple sign and minuses allowed
-        .gsub(/\+{2,}/, '+')         # replaces '+++' with '+'
-        .gsub(/\-{2,}/, '-')         # replaces '---' with '-'
-        .gsub(/\*{2,}/, '*')         # replaces '***' with '*'
-        .gsub(/\.{2,}/, '.')         # replaces '...' with '.'
-        .gsub(/[\+\-\*\.]{1,}$/, '') # remove pluses, minuses, dots and multiple signs at end of string
+        .gsub(/[^0-9+\-.*]+/, '') # only digits, dots, pluses, multiple sign and minuses allowed
+        .gsub(/\+{2,}/, '+')      # replaces '+++' with '+'
+        .gsub(/-{2,}/, '-')       # replaces '---' with '-'
+        .gsub(/\*{2,}/, '*')      # replaces '***' with '*'
+        .gsub(/\.{2,}/, '.')      # replaces '...' with '.'
+        .gsub(/[+\-*.]{1,}$/, '') # remove pluses, minuses, dots and multiple signs at end of string
 
       (eval(string) || 0).to_d
     end
