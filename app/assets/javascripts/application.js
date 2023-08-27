527cd70737c2f5ac437d8577c719bcce25a24260
Oleksandr Tkachuk <tksasha.me@gmail.com>
2023-08-27 11:53:45 +0300

    fixed bug with balance CSS class

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,9 +36,9 @@ $(function() {
     const value = parseFloat(this.innerHTML);
 
     if(value > 0) {
-      this.classList.add('fw-bold', 'text-success');
+      this.classList = ['sum fw-bold text-success'];
     } else if(value < 0) {
-      this.classList.add('fw-bold', 'text-danger');
+      this.classList = ['sum fw-bold text-danger'];
     } else {
       this.classList = ['sum'];
     }
