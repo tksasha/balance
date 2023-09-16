020939864a1bde637907d4ebb62343777641045e
Oleksandr Tkachuk <tksasha.me@gmail.com>
2023-09-16 10:42:17 +0300

    UI improvement

--- app/assets/javascripts/cashes.js
+++ app/assets/javascripts/cashes.js
@@ -4,6 +4,16 @@ $(function() {
       const table = new Table(element);
 
       table.summarize();
+
+      if($('#cashes [class|="col"]').size() == 2) {
+        const col = $('#cashes [class|="col"]:last');
+
+        if(col.find('.summary .sum').text().length > 9) {
+          col
+            .removeClass('col-3')
+            .addClass('col-4');
+        }
+      }
     });
   });
 });
