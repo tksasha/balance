1654b729cc3923c464c679841bd4fb740443fdcf
Oleksandr Tkachuk <tksasha.me@gmail.com>
2024-05-14 09:38:58 +0300

    updated bootstrap-datepicker to v1.10.0

--- vendor/assets/javascripts/bootstrap-datepicker.js
+++ vendor/assets/javascripts/bootstrap-datepicker.js
@@ -1,21 +1,8 @@
-/* =========================================================
- * bootstrap-datepicker.js
- * Repo: https://github.com/uxsolutions/bootstrap-datepicker/
- * Demo: https://uxsolutions.github.io/bootstrap-datepicker/
- * Docs: https://bootstrap-datepicker.readthedocs.org/
- * =========================================================
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+/*!
+ * Datepicker for Bootstrap v1.10.0 (https://github.com/uxsolutions/bootstrap-datepicker)
  *
- * https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * ========================================================= */
+ * Licensed under the Apache License v2.0 (https://www.apache.org/licenses/LICENSE-2.0)
+ */
 
 (function(factory){
     if (typeof define === 'function' && define.amd) {
