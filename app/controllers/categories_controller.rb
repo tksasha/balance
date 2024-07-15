558f9b292e6eb1263f1398d7351c4f699ad4c758
Oleksandr Tkachuk <tksasha.me@gmail.com>
2024-07-15 16:22:49 +0300

    added `POST /:currency/categories.js`

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,19 @@
 class CategoriesController < ApplicationController
   before_action :initialize_resource, only: :new
 
+  before_action :build_resource, only: :create
+
+  before_action -> { response.status = :created }, only: :create
+
   def new; end
+
+  def create
+    respond_to do |format|
+      format.js do
+        render :new, status: :unprocessable_entity unless resource.save
+      end
+    end
+  end
 
   private
 
@@ -21,5 +33,13 @@ class CategoriesController < ApplicationController
 
   def initialize_resource
     @resource = Category.new
+  end
+
+  def resource_params
+    params.require(:category).permit(:name).merge(currency: params[:currency])
+  end
+
+  def build_resource
+    @resource = Category.new(resource_params)
   end
 end
--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -17,5 +17,31 @@ RSpec.describe 'Categories' do
 
       it_behaves_like 'new.js'
     end
+
+    describe 'POST create.js' do
+      context 'when params are invalid' do
+        let(:params) { { category: { name: '' } } }
+
+        before { post "/#{ currency }/categories", params:, xhr: true }
+
+        it_behaves_like 'new.js'
+      end
+
+      context 'when params are valid' do
+        let(:name) { Faker::Commerce.product_name }
+
+        let(:params) { { category: { name: } } }
+
+        let(:category) { Category.last }
+
+        before { post "/#{ currency }/categories", params:, xhr: true }
+
+        it_behaves_like 'create.js'
+
+        it { expect(category.name).to eq name }
+
+        it { expect(category.currency).to eq currency }
+      end
+    end
   end
 end
