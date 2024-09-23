a75345eecac1ffe336cbace99ac5016c8006359a
Oleksandr Tkachuk <tksasha.me@gmail.com>
2024-09-23 17:49:49 +0300

    prettified decorator

--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.23.1
 require (
 	github.com/joho/godotenv v1.5.1
 	github.com/mattn/go-sqlite3 v1.14.23
+	golang.org/x/text v0.18.0
 )
--- internal/decorators/item_decorator.go
+++ internal/decorators/item_decorator.go
@@ -4,15 +4,21 @@ import (
 	"time"
 
 	"github.com/tksasha/balance/internal/models"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
 )
 
 type ItemDecorator struct {
-	item *models.Item
+	item    *models.Item
+	printer *message.Printer
 }
 
 func NewItemDecorator(item *models.Item) *ItemDecorator {
 	return &ItemDecorator{
 		item: item,
+		printer: message.NewPrinter(
+			language.Ukrainian,
+		),
 	}
 }
 
@@ -20,8 +26,8 @@ func (d *ItemDecorator) Date() string {
 	return d.item.Date.Format(time.DateOnly)
 }
 
-func (d *ItemDecorator) Sum() float32 {
-	return d.item.Sum
+func (d *ItemDecorator) Sum() string {
+	return d.printer.Sprintf("%.02f", d.item.Sum)
 }
 
 func (d *ItemDecorator) Category() string {
