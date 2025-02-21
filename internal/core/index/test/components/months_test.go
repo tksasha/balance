a4070b1c8c16ff2ef5cfe51c10865cec1f6bc163
Oleksandr Tkachuk <tksasha.me@gmail.com>
2025-02-21 14:26:47 +0200

    tests for months and years

--- internal/core/index/test/components/months_test.go
+++ internal/core/index/test/components/months_test.go
@@ -13,27 +13,26 @@ import (
 func TestMonths(t *testing.T) {
 	component := components.NewMonthsComponent(component.New())
 
-	t.Run("renders months.html for empty request", func(t *testing.T) {
+	testdata := map[string]string{
+		"":    "months.html",
+		"uah": "months-uah.html",
+		"usd": "months-usd.html",
+		"eur": "months-eur.html",
+	}
+
+	for currency, filename := range testdata {
 		writer := bytes.NewBuffer([]byte{})
 
 		values := url.Values{}
 
-		if err := component.Months(values).Render(writer); err != nil {
-			t.Fatalf("failed to render months: %v", err)
+		if currency != "" {
+			values.Add("currency", currency)
 		}
-
-		golden.Assert(t, writer.String(), "months.html")
-	})
-
-	t.Run("renders months-usd.html when currency is usd", func(t *testing.T) {
-		writer := bytes.NewBuffer([]byte{})
-
-		values := url.Values{"currency": {"usd"}}
 
 		if err := component.Months(values).Render(writer); err != nil {
 			t.Fatalf("failed to render months: %v", err)
 		}
 
-		golden.Assert(t, writer.String(), "months-usd.html")
-	})
+		golden.Assert(t, writer.String(), filename)
+	}
 }
