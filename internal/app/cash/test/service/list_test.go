378ad910dc28dd6ee5e9923100eda268b9100d09
Oleksandr Tkachuk <tksasha.me@gmail.com>
2025-02-24 10:58:54 +0200

    added test for list cash service

--- internal/app/cash/test/service/list_test.go
+++ internal/app/cash/test/service/list_test.go
@@ -1,10 +1,42 @@
 package service_test
 
 import (
-	"log"
+	"errors"
+	"slices"
 	"testing"
+
+	"github.com/tksasha/balance/internal/app/cash"
+	"github.com/tksasha/balance/internal/app/cash/service"
+	"github.com/tksasha/balance/internal/app/cash/test/mocks"
+	"go.uber.org/mock/gomock"
+	"gotest.tools/v3/assert"
 )
 
 func TestList(t *testing.T) {
-	log.Print("TODO: implement me -- !!!")
+	controller := gomock.NewController(t)
+
+	cashRepository := mocks.NewMockRepository(controller)
+
+	service := service.New(cashRepository)
+
+	ctx := t.Context()
+
+	t.Run("returns error when list cashes returns error", func(t *testing.T) {
+		cashRepository.EXPECT().List(ctx).Return(nil, errors.New("list cashes error"))
+
+		_, err := service.List(ctx)
+
+		assert.Error(t, err, "list cashes error")
+	})
+
+	t.Run("returns cashes when list cashes doesn't return error", func(t *testing.T) {
+		expected := cash.Cashes{}
+
+		cashRepository.EXPECT().List(ctx).Return(expected, nil)
+
+		actual, err := service.List(ctx)
+
+		assert.NilError(t, err)
+		assert.Assert(t, slices.Equal(actual, expected))
+	})
 }
