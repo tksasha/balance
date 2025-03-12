c871e056c48dcf6ab133af95fccb2cb5be79ec68
Oleksandr Tkachuk <tksasha.me@gmail.com>
2025-03-12 17:47:03 +0200

    added colors to balance

--- internal/app/balance/component/balance.go
+++ internal/app/balance/component/balance.go
@@ -2,11 +2,18 @@ package component
 
 import (
 	"github.com/tksasha/balance/internal/app/balance"
-	. "maragu.dev/gomponents"      //nolint:stylecheck
-	. "maragu.dev/gomponents/html" //nolint:stylecheck
+	. "maragu.dev/gomponents"            //nolint:stylecheck
+	. "maragu.dev/gomponents/components" //nolint:stylecheck
+	. "maragu.dev/gomponents/html"       //nolint:stylecheck
 )
 
 func (c *Component) Balance(balance *balance.Balance) Node {
+	classes := Classes{
+		"sum":          true,
+		"text-danger":  balance.Balance < 0,
+		"text-success": balance.Balance > 0,
+	}
+
 	return Div(Class("col-3"), ID("balance"),
 		Div(Class("card cash"),
 			Div(Class("card-body"),
@@ -24,7 +31,7 @@ func (c *Component) Balance(balance *balance.Balance) Node {
 							Td(
 								Text("Баланс"),
 							),
-							Td(Class("sum"),
+							Td(classes,
 								Text(c.Money(balance.Balance)),
 							),
 						),
