e78c9a6df261f40bb84b4f6eaf9c7563550dad53
Oleksandr Tkachuk <tksasha.me@gmail.com>
2025-03-18 12:40:27 +0200

    get /backoffice/categories/{id}/edit

--- internal/backoffice/category/component/edit.go
+++ internal/backoffice/category/component/edit.go
@@ -2,12 +2,24 @@ package component
 
 import (
 	"github.com/tksasha/balance/internal/backoffice/category"
-	. "maragu.dev/gomponents"      //nolint: stylecheck
+	"github.com/tksasha/balance/internal/common/component/path"
+	. "maragu.dev/gomponents" //nolint: stylecheck
+	htmx "maragu.dev/gomponents-htmx"
 	. "maragu.dev/gomponents/html" //nolint: stylecheck
 )
 
-func (c *Component) Edit(category *category.Category) Node {
+func (c *Component) Edit(params path.Params, category *category.Category) Node {
 	return Div(
+		c.Breadcrumbs(
+			Li(Class("breadcrumb-item"),
+				Span(Class("link"),
+					htmx.Get(path.BackofficeCategories(params)),
+					htmx.Target("#modal-body"),
+					Text("Категорії"),
+				),
+			),
+			Li(Class("breadcrumb-item active"), Text("Редагування")),
+		),
 		c.form(category, nil),
 	)
 }
--- internal/backoffice/category/handlers/edit.go
+++ internal/backoffice/category/handlers/edit.go
@@ -2,9 +2,11 @@ package handlers
 
 import (
 	"net/http"
+	"net/url"
 
 	"github.com/tksasha/balance/internal/backoffice/category"
 	"github.com/tksasha/balance/internal/backoffice/category/component"
+	"github.com/tksasha/balance/internal/common/component/path"
 	"github.com/tksasha/balance/internal/common/handler"
 )
 
@@ -33,11 +35,19 @@ func (h *EditHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = h.component.Edit(category).Render(w)
-
-	h.SetError(w, err)
+	h.ok(w, r.URL.Query(), category)
 }
 
 func (h *EditHandler) handle(r *http.Request) (*category.Category, error) {
 	return h.categoryService.Edit(r.Context(), r.PathValue("id"))
+}
+
+func (h *EditHandler) ok(w http.ResponseWriter, values url.Values, category *category.Category) {
+	params := path.Params{
+		"currency": values.Get("currency"),
+	}
+
+	err := h.component.Edit(params, category).Render(w)
+
+	h.SetError(w, err)
 }
