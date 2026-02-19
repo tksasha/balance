698d23f8aad09e7843e5d2d9efd823b240515704
Oleksandr Tkachuk <tksasha.me@gmail.com>
2026-02-19 09:24:50 +0200

    updated dependencies

--- internal/server/middlewares/log.go
+++ internal/server/middlewares/log.go
@@ -23,7 +23,7 @@ func (m *logMiddleware) Wrap(next http.Handler) http.Handler {
 
 		next.ServeHTTP(wrapper, r)
 
-		slog.Info(
+		slog.Info( //nolint:gosec
 			"request",
 			"method", r.Method,
 			"url", r.URL.String(),
